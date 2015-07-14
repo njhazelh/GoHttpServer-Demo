4675aab95c4186670691de566ea388064a48c05f
Nick Jones <nick@clypd.com>
2015-07-14 12:15:09 -0400

    Updated demo to work with HttpResponse

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"main/httpserver"
-	"net"
 )
 
 type HelloHandler struct {
@@ -14,23 +13,18 @@ func (h HelloHandler) String() string {
 	return "HelloHandler"
 }
 
-func generateHeader(contentLength int) string {
-	head := "HTTP/1.1 200 OK\r\n"
-	head += fmt.Sprintf("Content-length: %d\r\n", contentLength)
-	return head
-}
-
-func (h HelloHandler) Handle(r *httpserver.HttpRequest, c net.Conn) error {
-	content := "Hello, " + h.name
-	header := generateHeader(len(content))
-	message := header + "\r\n" + content + "\r\n"
-	fmt.Fprint(c, message)
-	return nil
+func (h HelloHandler) Handle(req *httpserver.HttpRequest, res *httpserver.HttpResponse) {
+	body := "Hello, " + h.name
+	res.SetStatus("OK")
+	fmt.Fprint(res, body)
+	fmt.Fprint(res, "\nThis is another line")
 }
 
 func main() {
 	server := httpserver.NewServer()
 	server.AddHandle("/", HelloHandler{"Bob"})
+	server.AddHandle("/frank", HelloHandler{"Frank"})
+	server.AddHandle("/russel", HelloHandler{"Russel"})
 	fmt.Printf("%v\n", server.Handlers["/"].String())
 	server.Run(":8080")
 }
